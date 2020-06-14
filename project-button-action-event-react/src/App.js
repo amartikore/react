461396762df15a6922daac3fc61fbdbae2df65bc
amartikore <amar.h.tikore123@gmail.com>
2020-06-14 13:00:17 +0530

    project-button-action-event-react : added 'btnclicked' button click action event listner for the button 'Click Me'

--- project-button-action-event-react/src/App.js
+++ project-button-action-event-react/src/App.js
@@ -3,20 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //button event listner function 'btnClicked'
+  //This function will get excuted evertime, when 'Click Me'button gets clicked.
+  const  btnclicked = ()=> {
+  alert('btn Clicked');
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello World</h1>
         <p>Welocme to my brand new react application</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={btnclicked}>Click Me</button>
       </header>
     </div>
   );
